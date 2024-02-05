c85abcf0b5000eee630a2fdc4e02579b502c8f69
dnspascal <dnspascal094@gmail.com>
2024-02-05 20:57:24 +0300

    main with bottom navigation

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -2,23 +2,35 @@ import { View, Text } from 'react-native'
 import React, { useState } from 'react'
 import AuthenticationComponent from '../components/AuthenticationComponent'
 import FingerprintAuthentication from '../components/FingerprintAuthentication'
+import FaceIdAuthentication from '../components/FaceIdAuthentication'
 
-const Login = () => {
-  const [selectedAuth,setSelectedAuth] = useState("fingerprint")
+const Login = ({navigation}) => {
+  const [selectedAuth, setSelectedAuth] = useState("fingerprint")
   
   return (
-    <View className=''>
-     <View className='h-[25%] bg-gray-400'>
+    <View>
+     <View className='h-[20%] bg-gray-300'>
 
      </View>
-     <View className='h-[75%]'>
-      <Text className='font-bold text-4xl text-center mt-5 text-black'>Welcome back</Text>
+     <View className='h-[80%] gap-y-8 items-center p-6'>
+      <Text className='font-bold text-4xl mt-5 text-black'>Welcome back</Text>
+    
+
       {
-        selectedAuth === "fingerprint" ? <FingerprintAuthentication/> : selectedAuth === "faceid" ? <Text>Faceid</Text> : <Text>Pin</Text>
+
+        selectedAuth === "fingerprint" ? <FingerprintAuthentication/> 
+        :
+         selectedAuth === "faceid" 
+         ? 
+      <FaceIdAuthentication navigation={navigation}/>
+        :
+         <Text className="text-black">Pin</Text>
       }
-      <Text className='text-xs text-center my-2 font-bold'>Authentication option</Text>
+      <Text className='text-xs text-center py-4 font-bold text-black'>Authentication option</Text>
+
      <AuthenticationComponent selectedAuth={selectedAuth} setSelectedAuth={setSelectedAuth} />
-     </View>
+     
+    </View>
     </View>
   )
 }
--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import React, { useLayoutEffect } from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
+import Bundle from './Bundle'
+import Services from './Services'
+import Account from './Account'
+import HomePage from './HomePage'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import {faHouse} from '@fortawesome/free-solid-svg-icons'
+import {faPuzzlePiece} from '@fortawesome/free-solid-svg-icons'
+import {faCube} from '@fortawesome/free-solid-svg-icons'
 
-const Main = () => {
+
+
+
+
+const Stack = createBottomTabNavigator() 
+const Main = ({navigation}) => {
+    useLayoutEffect(()=>{
+   navigation.setOptions({
+    headerShown:false,
+   })
+    },[])
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <Stack.Navigator>
+        <Stack.Screen 
+        component={HomePage} 
+        name="Home" 
+        options={{tabBarIcon:()=> <FontAwesomeIcon icon={faHouse} />}}
+        />
+        <Stack.Screen 
+        component={Bundle} 
+        name="Bundle"
+        options={{tabBarIcon:()=> <FontAwesomeIcon icon={faPuzzlePiece} />}}
+        />
+        <Stack.Screen 
+        component={Services} 
+        name="Services"
+        options={{tabBarIcon:()=> <FontAwesomeIcon icon={faCube} />}}
+        />
+        <Stack.Screen
+         component={Account}
+          name="Account"
+          options={{tabBarIcon:()=> <FontAwesomeIcon icon={faUser} />}}
+          />
+
+
+    </Stack.Navigator>
   )
 }
 
